483da3ef16145d016c48958126626d5a8511f57e
PrakharAgarwal135 <prakharagarwal135@gmail.com>
2024-11-29 17:50:17 +0530

    cart

--- src/components/core/Dashboard/Cart/index.jsx
+++ src/components/core/Dashboard/Cart/index.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
-export default function index() {
-  return <div></div>;
+import RenderCartCourses from "./RenderCartCourses";
+import RenderTotalAmount from "./RenderTotalAmount";
+
+export default function Cart() {
+  const { total, totalItems } = useSelector((state) => state.cart);
+
+  return (
+    <>
+      <h1 className="mb-8 text-3xl font-medium text-white">Cart</h1>
+      <p className="border-b border-b-richblack-400 pb-2 font-semibold text-richblack-400">
+        {totalItems} Courses in Cart
+      </p>
+      {total > 0 ? (
+        <div className="mt-8 flex flex-col-reverse items-start gap-x-10 gap-y-6 lg:flex-row">
+          <RenderCartCourses />
+          <RenderTotalAmount />
+        </div>
+      ) : (
+        <p className="mt-14 text-center text-3xl text-richblack-100">
+          Your cart is empty
+        </p>
+      )}
+    </>
+  );
 }
